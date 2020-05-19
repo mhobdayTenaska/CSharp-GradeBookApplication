cb7869aebdb6e43e5713bf8c68ed36749ff9bf03
Max Hobday <mhobday@tenaska.com>
2020-05-19 13:59:53 -0500

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,8 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +12,31 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            if(averageGrade >= grades[(int)Math.Ceiling(grades.Count * .2)])
+            {
+                return 'A';
+            }
+            else if (averageGrade >= grades[(int)Math.Ceiling(grades.Count * .4)])
+            {
+                return 'B';
+            }
+            else if (averageGrade >= grades[(int)Math.Ceiling(grades.Count * .6)])
+            {
+                return 'C';
+            }
+            else if (averageGrade >= grades[(int)Math.Ceiling(grades.Count * .8)])
+            {
+                return 'D';
+            }
+            return 'F';
         }
     }
 }
